ce940ae45b961973cd1b626725b091a6bbe53f2c
Venkatesh.Medisetti <mvenki2014@gmail.com>
2023-12-06 14:34:29 +0530

    PODS - 2/3 working

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -13,3 +13,6 @@ fi
 echo "REGISTRY_BASE_PATH: $REGISTRY_BASE_PATH"
 
 cat ci-app.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
+cat phpmyadmin.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
+cat mysql-storage.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
+cat mysql.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
--- scripts/build-ci-app.sh
+++ scripts/build-ci-app.sh
@@ -3,11 +3,10 @@
 source scripts/variables.sh
 
 echo "Build ci-app #$COMMIT and latest"
-
-cd Docker/ci-app
+cd ci-app
 docker image build -f "Dockerfile" . \
   --build-arg "app_name=ci-app" \
   -t "$REGISTRY_BASE_PATH/ci-app:${COMMIT}" \
   -t "$REGISTRY_BASE_PATH/ci-app:latest"
-cd ..
+
 cd ..
