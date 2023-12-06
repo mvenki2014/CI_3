9fa4f80aaaf586dd3a4f8d213b9db23e411fa0d0
Venkatesh.Medisetti <mvenki2014@gmail.com>
2023-12-06 17:38:37 +0530

    Perfectly Working phpmyadmin and Mysql connection

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -12,7 +12,7 @@ else
 fi
 echo "REGISTRY_BASE_PATH: $REGISTRY_BASE_PATH"
 
-cat ci-app.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
+#cat ci-app.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
 cat phpmyadmin.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
 cat mysql-storage.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
 cat mysql.yaml | sed "s/{{COMMIT}}/$COMMIT/g" | sed "s/{{REGISTRY_BASE_PATH}}/$REGISTRY_BASE_PATH/g" | kubectl apply -f-
