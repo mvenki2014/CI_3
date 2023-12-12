80889107060147305cabc9a0dad58b32c602960e
Venkatesh.Medisetti <mvenki2014@gmail.com>
2023-12-12 14:25:40 +0530

    updated cmd

--- scripts/build-ci-app.sh
+++ scripts/build-ci-app.sh
@@ -3,17 +3,16 @@
 source scripts/variables.sh
 
 echo "Build and push ci-app #$COMMIT and latest"
-cd ci-app
+#cd ci-app
 #docker image build -f "Dockerfile" . \
 #  --build-arg "app_name=ci-app" \
 #  -t "$REGISTRY_BASE_PATH/ci-app:${COMMIT}" \
 #  -t "$REGISTRY_BASE_PATH/ci-app:latest"
 
-
-az acr build --file "Dockerfile" \
-  --build-arg "app_name=ci-app" \
-  --image "$REGISTRY_BASE_PATH/ci-app:${COMMIT}" \
-  --image "$REGISTRY_BASE_PATH/ci-app:latest" \
-  --registry "$REGISTRY_BASE_PATH" \
-
+az acr build \
+		--image "$REGISTRY_BASE_PATH/ci-app:${COMMIT}" \
+		--image "$REGISTRY_BASE_PATH/ci-app:latest" \
+		--registry "$REGISTRY_BASE_PATH" \
+		--file "ci-app/Dockerfile" \
+		--build-arg "app_name=ci-app" \
 cd ..
