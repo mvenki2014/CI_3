6c676640b3dc4ae92dbbd61499fbc66d694c6e80
Venkatesh.Medisetti <mvenki2014@gmail.com>
2023-12-12 14:19:19 +0530

    updated reg name

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -5,7 +5,7 @@ COMMIT=$(git rev-parse --verify HEAD)
 echo "Commit #$COMMIT"
 
 if [ -z "${REGISTRY_BASE_PATH+x}" ]; then
-  export REGISTRY_BASE_PATH="myregistry.azurecr.io"
+  export REGISTRY_BASE_PATH="waazcr.azurecr.io"
 else
   export REGISTRY_BASE_PATH="$REGISTRY_BASE_PATH"
 fi
